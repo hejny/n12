78a71f6f85389b29eed7e8093e34c454ecd88945
Pavol Hejny <me@pavolhejny.com>
2021-06-24 13:04:56 +0200

    Main export

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// 🏭 GENERATED WITH generate-main-exports
+// ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+
+import { IKeywords } from './IKeywords';
+import { nameToUriPart } from './nameToUriPart';
+import { normalizeToCamelCase } from './normalizeToCamelCase';
+import { normalizeToSCREAMING_CASE } from './normalizeToSCREAMING_CASE';
+import { parseKeywords } from './parseKeywords';
+import { removeDiacritics } from './removeDiacritics';
+import { searchKeywords } from './searchKeywords';
+
+export {
+IKeywords,
+parseKeywords,
+nameToUriPart,
+searchKeywords,
+removeDiacritics,
+normalizeToCamelCase,
+normalizeToSCREAMING_CASE
+};
