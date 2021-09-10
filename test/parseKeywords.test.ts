03ec32f05987fe9cb7b93b2e3b9ca43fb0440170
Pavol Hejny <me@pavolhejny.com>
2021-09-10 21:46:04 +0200

    More tests for parsing of keywords

--- test/parseKeywords.test.ts
+++ test/parseKeywords.test.ts
@@ -23,12 +23,20 @@ describe('how parsing of keywords works', () => {
 
     it('can parse keywords from arrays', () => {
         expect(parseKeywords(['foo'])).toEqual(['foo']);
-        expect(parseKeywords(['foo bar', 'bzz'])).toEqual(['foo', 'bar', 'bzz']);
+        expect(parseKeywords(['foo bar', 'bzz'])).toEqual([
+            'foo',
+            'bar',
+            'bzz',
+        ]);
     });
 
     it('can parse keywords from objects', () => {
         expect(parseKeywords({ a: 'foo' })).toEqual(['foo']);
-        expect(parseKeywords({ b: ['foo bar', 'bzz'] })).toEqual(['foo', 'bar', 'bzz']);
+        expect(parseKeywords({ b: ['foo bar', 'bzz'] })).toEqual([
+            'foo',
+            'bar',
+            'bzz',
+        ]);
     });
 
     it('will strip diacritics from the keywords', () => {
@@ -42,13 +50,30 @@ describe('how parsing of keywords works', () => {
         expect(parseKeywords(`---aaá+++úůu:h`)).toEqual([`aaa`, `uuu`, `h`]);
     });
 
+    it('can parse keywords from strings with special chars', () => {
+        expect(parseKeywords('foo:')).toEqual(['foo']);
+        expect(parseKeywords('foo->bar')).toEqual(['foo', 'bar']);
+        expect(parseKeywords('foo: bar**hello--?worlD')).toEqual([
+            'foo',
+            'bar',
+            'hello',
+            'world',
+        ]);
+    });
+
     it('will make keywords lowercase', () => {
         expect(parseKeywords(`ěsčŘŽYÁÍÉuu`)).toEqual([`escrzyaieuu`]);
     });
 
     it('can parse numecic keywords', () => {
         expect(parseKeywords(['foo 123'])).toEqual(['foo', '123']);
-        expect(parseKeywords(['foo 0 bar 1', 'bzz'])).toEqual(['foo', '0', 'bar', '1', 'bzz']);
+        expect(parseKeywords(['foo 0 bar 1', 'bzz'])).toEqual([
+            'foo',
+            '0',
+            'bar',
+            '1',
+            'bzz',
+        ]);
         // TODO: What about decimal and negative numbers
     });
 });
